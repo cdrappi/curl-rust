0629cc5637aea4ec15c7e08b3192aa6bc6335d81
Ubuntu <azureuser@yocto-builder-3.3z0tm1yprcsefaq3x0wq2wcmyg.cx.internal.cloudapp.net>
2024-10-31 21:04:42 +0000

    try this

--- curl-sys/build.rs
+++ curl-sys/build.rs
@@ -4,6 +4,7 @@ use std::path::{Path, PathBuf};
 use std::process::Command;
 
 fn main() {
+    println!("cargo:warning=compiling libcurl from source");
     println!("cargo:rerun-if-changed=curl");
     println!(
         "cargo:rustc-check-cfg=cfg(\
